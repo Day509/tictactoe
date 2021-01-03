ba69eb2a433ff3193dcf09f76a7af6d6dac0ebfe
Day509 <72015298+Day509@users.noreply.github.com>
2021-01-03 16:45:48 +0100

    Add files via upload

--- J1vsCPU.h
+++ J1vsCPU.h
@@ -2,56 +2,65 @@
 #include <stdlib.h>
 #include <time.h>
 
-int gagnant(int *(tab)[3],int joueur[])
+void affichage(int (*tab)[3])
 {
-	for (int j = 0; j < 3; j++)
+	for(int i=0 ;i < 3; i++)
 	{
-		for (int i = 0; i < 3; i++)
+		for(int j = 0;j < 3; j++)
 		{
-			if ((i < 3) && (j == 0))
+			printf("[%d]", tab[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+int win(int (*tab)[3],int joueur[])
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if ((i < 3) && (j == 0))//Vérifie un gagnant à l'horizontale 
 			{
 				if((tab[i][j] == joueur[0]) && (tab[i][j+1] == joueur[0]) && (tab[i][j+2] == joueur[0]))
 				{
-
 					return 1;
 				}
 				else if((tab[i][j] == joueur[1]) && (tab[i][j+1] == joueur[1]) && (tab[i][j+2] == joueur[1]))
 				{
-
 					return 1;
 				}
 			}
-			if ((i == 0) && (j < 3))
-			{
-				if((tab[i][j] == joueur[1]) && (tab[i+1][j] == joueur[1]) && (tab[i+2][j] == joueur[1]))
-				{
-
-					return 1;
-				}
-			}
-			if ((i == 0) && (j == 0))
+			if ((i == 0) && (j < 3))//Vérifie un gagnant à la verticale
 			{
 				if((tab[i][j] == joueur[0]) && (tab[i+1][j] == joueur[0]) && (tab[i+2][j] == joueur[0]))
 				{
-
 					return 1;
 				}
-				else if((tab[i][j] == joueur[1]) && (tab[i+1][j] == joueur[1]) && (tab[i+2][j] == joueur[1]))
+				if((tab[i][j] == joueur[1]) && (tab[i+1][j] == joueur[1]) && (tab[i+2][j] == joueur[1]))
 				{
-
 					return 1;
 				}
 			}
-			if ((i == 2) && (j == 2))
+			if ((i == 0) && (j == 0))//Vérifie un gagnant en diagonale gauche-droite
 			{
-				if((tab[i][j] == joueur[0]) && (tab[i-1][j-1] == joueur[0]) && (tab[i-2][j-2] == joueur[0]))
+				if((tab[i][j] == joueur[0]) && (tab[i+1][j+1] == joueur[0]) && (tab[i+2][j+2] == joueur[0]))
 				{
-
 					return 1;
 				}
-				else if((tab[i][j] == joueur[1]) && (tab[i-1][j-1] == joueur[1]) && (tab[i-2][j-2] == joueur[1]))
+				else if((tab[i][j] == joueur[1]) && (tab[i+1][j+2] == joueur[1]) && (tab[i+2][j+2] == joueur[1]))
 				{
-
+					return 1;
+				}
+			}
+			if ((i == 2) && (j == 0))//Vérifie un gagnant en diagonale droite-gauche
+			{
+				if((tab[i][j] == joueur[0]) && (tab[i-1][j+1] == joueur[0]) && (tab[i-2][j+2] == joueur[0]))
+				{
+					return 1;
+				}
+				else if((tab[i][j] == joueur[1]) && (tab[i-1][j+1] == joueur[1]) && (tab[i-2][j+2] == joueur[1]))
+				{
 					return 1;
 				}
 			}
@@ -64,26 +73,111 @@ int gagnant(int *(tab)[3],int joueur[])
 
 void Presentation_JoueurvsCPU()
 {
+	srand(time(NULL));
 	int grille[3][3]={0};
 	int joueur[2]={1,2};
-	int x = joueur[0];
-	int compris = 0; fini = 0;
-	while(compris != 1){
-		printf("Bonjour\nBienvenue dans le mode Joueur VS CPU\n Le principe est simple, Vous devez taper les coordonnées de la case que vous voulez choisir puis appuyez sur entrer\nExemple: 1.3 correspond à la première colonne de la troisième ligne\n");
+	int i = 0; int j = 0; int a; int x;
+	int compris = 0; int fini = 0; int nul=0;
+	printf("Bonjour\nBienvenue dans le mode Joueur VS CPU\n");
+	while(compris != 1)
+	{
+		printf("Le principe est simple, Vous devez taper les coordonnées de la case que vous voulez choisir puis appuyez sur entrer\nExemple: 1.3 correspond à la troisième colonne de la première ligne\n\nEt n'oubliez pas le '.' entre les chiffres\n");
 		printf("Si vous avez tous compris tapez 1 : ");
 		scanf("%d", &compris);
 		printf("\n");
 	}
 
 	printf("Bien commençons\n");
+	a = rand()%2;
+	x = joueur[a];
+	if (x == 1)
+	{
+		printf("C'est le joueur %d qui commence\n", x);
+	}
+	else
+	{
+		printf("C'est l'ordinateur qui commence\n");
+	}
+	
 
 	while(fini !=1)
 	{
 		if (x == joueur[0])
 		{
-			/* code */
-		}
-		printf("Où voulez-vous placez votre pion?\n");
+			printf("Où voulez-vous placez votre pion?\n");
+			scanf("%d.%d", &i, &j);
+			printf("\nVoici i: %d, j: %d\n", i,j);
+			if((i >= 4) || (j >= 4))
+			{
+				while ((i >= 4) || (j >= 4))
+				{
+					printf("Votre saisie n'est pas bonne, veuillez réesayer\n");
+					scanf("%d.%d\n", &i, &j);
+				}
+			}
+			i = i-1; j = j-1;
 
+			while(grille[i][j] != 0)
+			{
+				printf("Cette case est déja prise, Veuillez saisir une autre case\n");
+				scanf("%d.%d\n", &i, &j);
+			}
+			grille[i][j] = x;
+			affichage(grille);
+			if(win(grille,joueur) == 1)
+			{
+				printf("Le joueur %d à gagner\n", x);
+				fini = 1;
+			}
+			else
+			{
+				printf("C'est au tour du CPU de jouer\n");
+				x = joueur[1];
+			}
+		}
+		else if (x == joueur[1])
+		{
+			i = rand()%3;
+			j = rand()%3;
+
+			while(grille[i][j] != 0)
+			{
+				i = rand()%3;
+				j = rand()%3;
+			}
+
+			grille[i][j] = x;
+			affichage(grille);
+			if(win(grille,joueur) == 1)
+			{
+				printf("Le joueur %d à gagner\n", x);
+				fini = 1;
+			}
+			else
+			{
+				printf("C'est au tour du joueur 1 de jouer\n");
+				x = joueur[0];
+			}
+			
+		}
+		for (int a = 0; a < 3; a++)
+		{
+			for (int b = 0; b < 3; b++)
+			{
+				if (grille[a][b] != 0)
+				{
+					nul += 1;
+				}
+			}
+		}
+		if (nul == 9)
+		{
+			printf("Il y a égalité\n");
+			fini =1;
+		}
+		else
+		{
+			nul = 0;
+		}
 	}
 }
